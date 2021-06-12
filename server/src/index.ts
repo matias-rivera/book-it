d9035901c9fa59764b6440f60b2155500b363033
Matias Rivera <juanmatiasrivera@outlook.com>
2021-06-12 04:08:47 -0300

    GraphQL Setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,33 +1,12 @@
 import express from "express";
-import { listings } from "./listings";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./graphql";
 
 const app = express();
-
 const port = 5000;
 
-app.use(express.json());
-
-app.get("/", (_req, res) => {
-    res.send("hello");
-});
-
-// Listing routes
-
-app.get("/listings", (_req, res) => {
-    return res.send(listings);
-});
-
-app.post("/listings-delete", (req, res) => {
-    const id: string = req.body.id;
-
-    for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-            return res.send(listings.splice(i, 1));
-        }
-    }
-
-    return res.send("failed");
-});
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port, () => {
     console.log(`running on port ${port}`);

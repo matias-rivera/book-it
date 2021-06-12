32ff9f2d4d8908a296cec2b43cb91881782ea09d
Matias Rivera <juanmatiasrivera@outlook.com>
2021-06-12 06:01:05 -0300

    mongodb setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,22 @@
-import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./graphql";
+import { connectDatabase } from "./database";
 
-const app = express();
-const port = 5000;
+const mount = async (app: Application) => {
+    const db = await connectDatabase();
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: () => ({ db }),
+    });
+    server.applyMiddleware({ app, path: "/api" });
+    app.listen(process.env.PORT, () => {
+        console.log(`running on port ${process.env.PORT}`);
+    });
+};
 
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app, path: "/api" });
-
-app.listen(port, () => {
-    console.log(`running on port ${port}`);
-});
+mount(express());

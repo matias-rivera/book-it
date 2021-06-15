f280790cdb929506a23379a80917c3cd2674bf23
Matias Rivera <juanmatiasrivera@outlook.com>
2021-06-15 13:30:55 -0300

    Login client-side

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,29 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { Layout } from "antd";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/index.css";
 
 /* Components */
-import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
+import {
+    Home,
+    Host,
+    Listing,
+    Listings,
+    NotFound,
+    User,
+    Login,
+} from "./sections";
+import { Viewer } from "./lib/types";
 
 const client = new ApolloClient({
     uri: "/api",
     cache: new InMemoryCache(),
 });
 
+const initialViewer: Viewer = {
+    id: null,
+    token: null,
+    avatar: null,
+    hasWallet: null,
+    didRequest: false,
+};
+
 const App = () => {
+    const [viewer, setViewer] = useState<Viewer>(initialViewer);
     return (
         <Router>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/host" component={Host} />
-                <Route exact path="/listing/:id" component={Listing} />
-                <Route exact path="/listings/:location?" component={Listings} />
-                <Route exact path="/user/:id" component={User} />
-                <Route component={NotFound} />
-            </Switch>
+            <Layout id="app">
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/host" component={Host} />
+                    <Route exact path="/listing/:id" component={Listing} />
+                    <Route
+                        exact
+                        path="/listings/:location?"
+                        component={Listings}
+                    />
+                    <Route
+                        exact
+                        path="/login"
+                        render={(props) => (
+                            <Login {...props} setViewer={setViewer} />
+                        )}
+                    />
+                    <Route exact path="/user/:id" component={User} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Layout>
         </Router>
     );
 };

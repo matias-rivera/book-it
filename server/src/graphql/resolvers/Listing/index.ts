718a37a8e6593d0438723c7596532ff08c7fefd3
Matias Rivera <juanmatiasrivera@outlook.com>
2021-07-20 20:40:11 -0300

    HostListing Resolver

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -1,7 +1,7 @@
 import { IResolvers } from "apollo-server-express";
 import { ObjectId } from "mongodb";
 import { Google } from "../../../lib/api";
-import { Database, Listing, User } from "../../../lib/types";
+import { Database, Listing, ListingType, User } from "../../../lib/types";
 import {
     ListingArgs,
     ListingBookingsArgs,
@@ -10,9 +10,31 @@ import {
     ListingsData,
     ListingsFilter,
     ListingsQuery,
+    HostListingArgs,
+    HostListingInput,
 } from "./types";
 import { authorize } from "../../../lib/utils";
 import { Request } from "express";
+
+const verifyHostListingInput = ({
+    title,
+    description,
+    type,
+    price,
+}: HostListingInput) => {
+    if (title.length > 100) {
+        throw new Error("Listing title must be under 100 characters");
+    }
+    if (description.length > 5000) {
+        throw new Error("Listing description must be under 5000 characters");
+    }
+    if (type !== ListingType.Apartment && type !== ListingType.House) {
+        throw new Error("Listing type must be either an apartment or house");
+    }
+    if (price < 0) {
+        throw new Error("Price must be greater than 0");
+    }
+};
 
 export const listingResolvers: IResolvers = {
     Query: {
@@ -87,6 +109,48 @@ export const listingResolvers: IResolvers = {
             } catch (error) {
                 throw new Error(`Failed to query listings: ${error}`);
             }
+        },
+    },
+    Mutation: {
+        hostListing: async (
+            _root: undefined,
+            { input }: HostListingArgs,
+            { db, req }: { db: Database; req: Request }
+        ): Promise<Listing> => {
+            verifyHostListingInput(input);
+
+            const viewer = await authorize(db, req);
+            if (!viewer) {
+                throw new Error("viewer cannot be found");
+            }
+
+            const { country, admin, city } = await Google.geocode(
+                input.address
+            );
+
+            if (!country || !admin || !city) {
+                throw new Error("Invalid address input");
+            }
+
+            const insertResult = await db.listings.insertOne({
+                _id: new ObjectId(),
+                ...input,
+                bookings: [],
+                bookingsIndex: {},
+                country,
+                admin,
+                city,
+                host: viewer._id,
+            });
+
+            const insertedListing: Listing = insertResult.ops[0];
+
+            await db.users.updateOne(
+                { _id: viewer._id },
+                { $push: { listings: insertedListing._id } }
+            );
+
+            return insertedListing;
         },
     },
     Listing: {
--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -73,6 +73,16 @@ export const typeDefs = gql`
         code: String!
     }
 
+    input HostListingInput {
+        title: String!
+        description: String!
+        image: String!
+        type: ListingType!
+        address: String!
+        price: Int!
+        numOfGuests: Int!
+    }
+
     type Query {
         authUrl: String!
         user(id: ID!): User!
@@ -90,5 +100,6 @@ export const typeDefs = gql`
         logOut: Viewer!
         connectStripe(input: ConnectStripeInput!): Viewer!
         disconnectStripe: Viewer!
+        hostListing(input: HostListingInput): Listing!
     }
 `;
